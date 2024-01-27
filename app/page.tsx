33372050a0a0bfec36268d48534e7b3ae6afbc2c
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2024-01-27 10:43:33 +0200

    solved a build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Blogs from "@/components/Blogs";
 import Hero from "@/components/Hero";
 import Cta from "@/components/Cta";
 
+const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
 export default async function Home() {
 
-  const res = await fetch(`${window.location.origin}/api/blogPosts`);
+  const res = await fetch(`${baseUrl}/api/blogPosts`);
   const posts = await res.json()
 
   return (
